f3f02ec5c265795a3b66356fcebc243ae105ad6a
Niranjan <niranjan.ramakrishna22@gmail.com>
2021-12-24 09:52:34 +0530

    fix

--- connections-profile-service/src/main/java/com/user/model/Like.java
+++ connections-profile-service/src/main/java/com/user/model/Like.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @ToString
 @Table(name="reaction")
+@Entity
 public class Like {
     @Id
     @GeneratedValue(generator = "like_seq",strategy = GenerationType.AUTO)
