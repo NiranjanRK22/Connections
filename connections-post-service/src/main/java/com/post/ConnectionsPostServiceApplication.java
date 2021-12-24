d6ada1c057e3bd030b4b77439147d8940e9b4302
nayeemahmedshaik <sheiknayeemahmed@gmail.com>
2021-12-24 10:17:10 +0530

    added post microservice

--- connections-post-service/src/main/java/com/post/ConnectionsPostServiceApplication.java
+++ connections-post-service/src/main/java/com/post/ConnectionsPostServiceApplication.java
@@ -1,13 +1,22 @@
 package com.post;
 
+import com.post.model.Post;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConnectionsPostServiceApplication {
+public class ConnectionsPostServiceApplication implements CommandLineRunner {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConnectionsPostServiceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+
+		Post post = new Post();
+	}
 }
